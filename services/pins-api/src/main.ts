891742996ea0380568831c27c75b0933740eb7b8
Kacper S <kacpersmal@gmail.com>
2025-06-05 22:26:45 +0200

    [Improvements] QOL API Improvements (#11)
    * add global exception filter
    
    * add healthchecks
    
    * create optimized image for cloud run
    
    * Update deploy-pins-api.yml

--- services/pins-api/src/main.ts
+++ services/pins-api/src/main.ts
@@ -2,9 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import ConfigureSwagger from 'src/utils/swagger-config';
 import { ValidationPipe } from '@nestjs/common';
-
+import { GlobalExceptionFilter } from 'src/shared/filters/global-exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new GlobalExceptionFilter());
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
     origin: '*',
--- services/pins-api/src/shared/shared.module.ts
+++ services/pins-api/src/shared/shared.module.ts
@@ -3,10 +3,13 @@ import { FirebaseService } from './services/firebase.service';
 import { FirestoreService } from './services/firestore.service';
 import { ConfigModule } from '@nestjs/config';
 import { StorageService } from 'src/shared/services/storage.service';
+import { HealthController } from './health.controller';
+import { TerminusModule } from '@nestjs/terminus';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), TerminusModule],
   providers: [FirebaseService, FirestoreService, StorageService],
   exports: [FirebaseService, FirestoreService, StorageService],
+  controllers: [HealthController],
 })
 export class SharedModule {}
