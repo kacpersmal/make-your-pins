fca44e79eedc17fc34933d80a42329069c3fde53
Adrian-Oz <adrianozorekv1@gmail.com>
2025-06-07 18:24:58 +0200

    Replace external image URL with a local asset.

--- services/client-app/src/components/auth/auth-form-side.tsx
+++ services/client-app/src/components/auth/auth-form-side.tsx
@@ -1,11 +1,11 @@
 export default function AuthFormSide() {
   return (
     <>
-      <div className="bg-muted  z-60 relative hidden md:block">
+      <div className="bg-muted   z-60 relative hidden md:block">
         <img
-          src="https://i.pinimg.com/1200x/c1/91/29/c1912995a8c183ab65b95174323aaa1e.jpg"
+          src="/form-side-image.jpg"
           alt="Image"
-          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+          className="absolute inset-0 h-full w-full  dark:brightness-[0.2] dark:grayscale"
         />
       </div>
     </>
